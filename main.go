2ca4fc9cd42b64db3d28b1766666e492f4123d0e
ushion0a <66307812+ushion0a@users.noreply.github.com>
2025-04-26 15:55:15 +0800

    Update main.go

--- main.go
+++ main.go
@@ -63,8 +63,8 @@ import (
 	//                               vvvv                               //
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/custom"                   // 自定义插件合集
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ahsai"             // ahsai tts
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aifalse"           // 服务器监控
+//	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ahsai"             // ahsai tts
+//	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aifalse"           // 服务器监控
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aiwife"            // 随机老婆
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/alipayvoice"       // 支付宝到账语音
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/animetrace"        // AnimeTrace 动画/Galgame识别
@@ -74,7 +74,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/base64gua"         // base64卦加解密
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/baseamasiro"       // base天城文加解密
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bilibili"          // b站相关
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bookreview"        // 哀伤雪刃吧推书记录
+//	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bookreview"        // 哀伤雪刃吧推书记录
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chess"             // 国际象棋
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/choose"            // 选择困难症帮手
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chouxianghua"      // 说抽象话
@@ -102,7 +102,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/hyaku"             // 百人一首
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/inject"            // 注入指令
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/jandan"            // 煎蛋网无聊图
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/jptingroom"        // 日语听力学习材料
+//	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/jptingroom"        // 日语听力学习材料
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/kfccrazythursday"  // 疯狂星期四
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/lolicon"           // lolicon 随机图片
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/lolimi"            // 桑帛云 API
@@ -121,10 +121,10 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/novel"             // 铅笔小说网搜索
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/nsfw"              // nsfw图片识别
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/nwife"             // 本地老婆
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/omikuji"           // 浅草寺求签
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/poker"             // 抽扑克
+	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/omikuji"           // 浅草寺求签
+	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/poker"             // 抽扑克
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/qqwife"            // 一群一天一夫一妻制群老婆
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/qzone"             // qq空间表白墙
+//	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/qzone"             // qq空间表白墙
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/realcugan"         // realcugan清晰术
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/reborn"            // 投胎
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/robbery"           // 打劫群友的ATRI币
